76cbadea13c8744d050770e4362dbcb7143ce179
Volodya Pabyrivkskyi <pabyrivskyi@gmail.com>
2023-04-21 15:05:14 +0300

    Add optimistic concurrency update test.

--- MassiveInsert/Program.cs
+++ MassiveInsert/Program.cs
@@ -3,11 +3,17 @@ using System.Diagnostics;
 
 var connString = "Server=localhost:5432;Database=postgres;User Id=postgres;Password=postgres;";
 
-//await RunTestWithExecutionTimeLogged("Read-Modify-Write: Lost Update Test", LostUpdate);
+await RunTestWithExecutionTimeLogged("Read-Modify-Write: Lost Update Test", LostUpdate);
 
 //await RunTestWithExecutionTimeLogged("In-place Update Test", InPlaceUpdate);
 
-await RunTestWithExecutionTimeLogged("Row-lovel Locking Update Test", RowLockUpdate);
+//await RunTestWithExecutionTimeLogged("Row-level Locking Update Test", RowLockUpdate);
+
+// Optimistic concurrency is super slow with 10 threads and 10_000 iterations each ~ 40min
+//await RunTestWithExecutionTimeLogged("Optimistic Concurrency Control Update Test", OptimisticConcurrencyUpdate);
+
+// Optimistic concurrency with 10 threads with 50_000 iterations per each is a way faster ~  4min. So, we can see the prove that Optimistic Concurrency has performance degradation with big number of concurrent updates.
+//await RunTestWithExecutionTimeLogged("Optimistic Concurrency Control Update Test", OptimisticConcurrencyUpdate, 2, 50_000);
 
 Console.ReadLine();
 
@@ -34,15 +40,36 @@ void RowLockUpdate(NpgsqlConnection conn)
     tx.Commit();
 }
 
+void OptimisticConcurrencyUpdate(NpgsqlConnection conn)
+{
+    while (true) 
+    {
+        int counter, version;
+        using (var cmdSelect = new NpgsqlCommand("SELECT Counter, Version FROM user_counter WHERE User_Id = 1", conn))
+        using (var reader = cmdSelect.ExecuteReader()) 
+        {
+            reader.Read();
+            counter = reader.GetInt32(0);
+            version = reader.GetInt32(1);
+        }
+
+        counter = counter + 1;
+
+        using var cmdUpdate = new NpgsqlCommand($"UPDATE user_counter SET Counter = { counter }, Version = { version + 1 } WHERE User_Id = 1 AND Version = { version }", conn);
+        if (cmdUpdate.ExecuteNonQuery() > 0)
+            break;
+    }
+}
+
 #region helpers
 
-async Task RunTestWithExecutionTimeLogged(string testName, Action<NpgsqlConnection> testFunc)
+async Task RunTestWithExecutionTimeLogged(string testName, Action<NpgsqlConnection> testFunc, int tasksCount = 10, int iterationsPerTaskCount = 10_000)
 {
     Console.WriteLine($"Starting: {testName}");
     Stopwatch stopwatch = new Stopwatch();
     stopwatch.Start();
 
-    var tasks = RunTasks(testFunc);
+    var tasks = RunTasks(testFunc, tasksCount, iterationsPerTaskCount);
 
     await Task.WhenAll(tasks);
 
@@ -53,22 +80,23 @@ async Task RunTestWithExecutionTimeLogged(string testName, Action<NpgsqlConnecti
     conn.Open();
     Console.WriteLine($"Counter: {GetCounter(conn)}");
     
-    if(UpdateCounter(conn, 0) > 0)
-        Console.WriteLine("Counter is reset to 0");
+    using var cmdReset = new NpgsqlCommand("UPDATE user_counter SET Counter = 0, Version = 0 WHERE User_Id = 1", conn);
+    if(cmdReset.ExecuteNonQuery() > 0)
+        Console.WriteLine("Counter and Version are reset to 0");
 
     Console.WriteLine();
 }
 
-IEnumerable<Task> RunTasks(Action<NpgsqlConnection> testFunc)
+IEnumerable<Task> RunTasks(Action<NpgsqlConnection> testFunc, int tasksCount, int iterationsPerTaskCount)
 {
     // Run 10 tasks, each of which executes test function 10_000 times.
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < tasksCount; i++)
         yield return Task.Run(() =>
         {
             using (var conn = new NpgsqlConnection(connString))
             {
                 conn.Open();
-                for (int j = 0; j < 10_000; j++)
+                for (int j = 0; j < iterationsPerTaskCount; j++)
                         testFunc(conn);
             }
         });
